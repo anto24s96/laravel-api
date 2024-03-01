7e8bfb09f23a2aaa9c63a24cb2d3a455c1d8c172
anto24s96 <anto24s96@gmail.com>
2024-03-01 16:04:55 +0100

    [UPDATE] milestone 1/2 completed

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -3,9 +3,18 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    //
+    public function index()
+    {
+        $projects = Project::with('type', 'technologies')->orderBy('id', 'desc')->paginate(5);
+
+        return response()->json([
+            'success' => true,
+            'results' => $projects
+        ]);
+    }
 }

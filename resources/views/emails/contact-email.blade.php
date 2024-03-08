7ff91273c299b4c6814d939e3477c3a627405d12
anto24s96 <anto24s96@gmail.com>
2024-03-08 15:15:11 +0100

    [UPDATE] create leads table

---
+++ resources/views/emails/contact-email.blade.php
@@ -0,0 +1,8 @@
+<h1>Ciao Admin</h1>
+<p>
+    Hai ricevuto una nuova email: <br>
+    Nome: {{ $lead->name }} <br>
+    Email: {{ $lead->email }} <br>
+    Messaggio: <br>
+    {{ $lead->content }}
+</p>

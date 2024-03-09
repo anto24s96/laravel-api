a7f9d134d192ec1d1935c51c40158e2c2ca4040e
anto24s96 <anto24s96@gmail.com>
2024-03-09 15:57:44 +0100

    [FIX] added some fixes

--- resources/views/admin/project/edit-project.blade.php
+++ resources/views/admin/project/edit-project.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.admin')
 
 @section('content')
     <div class="container">
@@ -10,25 +10,33 @@
 
         <div class="row justify-content-center py-3">
             <div class="col-8">
-                <div class="border border-3 rounded-2 p-3 border-danger">
-                    <form action="{{ route('admin.project.update', $project->id) }}" method="POST"
-                        enctype="multipart/form-data">
+                <div class="my-form p-3">
+                    <form action="{{ route('admin.project.update', $project->id) }}" method="POST" enctype="multipart/form-data">
                         @csrf
                         @method('PUT')
 
                         <div class="mb-3">
                             <label for="name" class="form-label">Name:</label>
-                            <input type="text" class="form-control @error('name') is-invalid @enderror" id="name"
-                                name="name" required value="{{ $project['name'] }}">
+                            <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" required value="{{ $project['name'] }}">
                             @error('name')
                                 <div class="text-danger">{{ $message }}</div>
                             @enderror
                         </div>
 
                         <div class="mb-3">
+                            <label for="logo" class="form-label">Logo:</label>
+                            @if ($project->logo != null)
+                                <img src="{{ asset('storage/' . $project->logo) }}" alt="cover_image" class="w-25">
+                            @endif
+                            <input type="file" class="form-control @error('logo') is-invalid @enderror mt-4" id="logo" name="logo" value="{{ $project['logo'] }}">
+                            @error('logo')
+                                <div class="text-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+
+                        <div class="mb-3">
                             <label for="type_id" class="form-label">Type:</label>
-                            <select class="form-select w-25 @error('type_id') is-invalid @enderror" name="type_id"
-                                id="type_id">
+                            <select class="form-select w-25 @error('type_id') is-invalid @enderror" name="type_id" id="type_id">
                                 <option value="">Select type</option>
                                 @foreach ($types as $type)
                                     <option value="{{ $type->id }}" @selected($type->id == old('type_id', $project->type ? $project->type->id : ''))>
@@ -41,21 +49,15 @@
                             @enderror
                         </div>
 
-                        <div class="form-group mb-3">
+                        <div class="form-group mb-4">
                             <label class="control-label">Select technology</label>
                             <div>
                                 @foreach ($technologies as $technology)
                                     <div class="form-check-inline">
                                         @if ($errors->any())
-                                            <input type="checkbox" name="technologies[]"
-                                                id="technology-{{ $technology->id }}" class="form-check-input"
-                                                value="{{ $technology->id }}"
-                                                {{ is_array($technology->id, old('technologies')) ? 'checked' : '' }}>
+                                            <input type="checkbox" name="technologies[]" id="technology-{{ $technology->id }}" class="form-check-input" value="{{ $technology->id }}" {{ is_array($technology->id, old('technologies')) ? 'checked' : '' }}>
                                         @else
-                                            <input type="checkbox" name="technologies[]"
-                                                id="technology-{{ $technology->id }}" class="form-check-input"
-                                                value="{{ $technology->id }}"
-                                                {{ $project->technologies->contains($technology->id) ? 'checked' : '' }}>
+                                            <input type="checkbox" name="technologies[]" id="technology-{{ $technology->id }}" class="form-check-input" value="{{ $technology->id }}" {{ $project->technologies->contains($technology->id) ? 'checked' : '' }}>
                                         @endif
 
                                         <label for="technologies" class="form-check-label">{{ $technology->name }}</label>
@@ -65,21 +67,8 @@
                         </div>
 
                         <div class="mb-3">
-                            <label for="logo" class="form-label">Logo:</label>
-                            @if ($project->logo != null)
-                                <img src="{{ asset('storage/' . $project->logo) }}" alt="cover_image" class="w-25">
-                            @endif
-                            <input type="file" class="form-control @error('logo') is-invalid @enderror" id="logo"
-                                name="logo" value="{{ $project['logo'] }}">
-                            @error('logo')
-                                <div class="text-danger">{{ $message }}</div>
-                            @enderror
-                        </div>
-
-                        <div class="mb-3">
                             <label for="description" class="form-label">Description:</label>
-                            <textarea class="form-control @error('description') is-invalid @enderror" id="textarea-description" rows="10"
-                                name="description" required>{{ $project['description'] }}</textarea>
+                            <textarea class="form-control @error('description') is-invalid @enderror" id="textarea-description" rows="10" name="description" required>{{ $project['description'] }}</textarea>
                             @error('description')
                                 <div class="text-danger">{{ $message }}</div>
                             @enderror
@@ -87,8 +76,7 @@
 
                         <div class="mb-3">
                             <label for="start_date" class="form-label">Start Date:</label>
-                            <input type="text" class="form-control @error('start_date') is-invalid @enderror"
-                                id="start_date" name="start_date" required value="{{ $project['start_date'] }}">
+                            <input type="text" class="form-control @error('start_date') is-invalid @enderror" id="start_date" name="start_date" required value="{{ $project['start_date'] }}">
                             @error('start_date')
                                 <div class="text-danger">{{ $message }}</div>
                             @enderror
@@ -96,16 +84,14 @@
 
                         <div class="mb-3">
                             <label for="end_date" class="form-label">End Date:</label>
-                            <input type="text" class="form-control @error('end_date') is-invalid @enderror"
-                                id="end_date" name="end_date" required value="{{ $project['end_date'] }}">
+                            <input type="text" class="form-control @error('end_date') is-invalid @enderror" id="end_date" name="end_date" required value="{{ $project['end_date'] }}">
                             @error('end_date')
                                 <div class="text-danger">{{ $message }}</div>
                             @enderror
                         </div>
 
                         <div class="text-center">
-                            <button type="submit"
-                                class="btn btn-outline-warning text-uppercase fw-bolder my-3">SUBMIT</button>
+                            <button type="submit" class="my_button fw-bolder my-3">SUBMIT</button>
                         </div>
                     </form>
                 </div>
